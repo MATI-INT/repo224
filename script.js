337bc85bba7df1a2f7546b27e5ebed42b5adf4bb
bodrovis <golosizpru@gmail.com>
2015-09-28 15:16:03 +0300

    initial

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('body').on('click', '.go-to', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var href = $this.attr('href'); // #my_block #my_block2
+    $this;
+    $('html').animate({
+      scrollTop: $(href).offset().top
+    }, 2000, function() {
+      document.location.hash = href;
+      $this;
+    });
+    $this;
+  });
+
+  $('#go-to-top').click(
+      function() {
+        $('html, body').animate({
+          scrollTop: $('#my_block').offset().top
+        }, 2000);
+      }
+  )
+});
